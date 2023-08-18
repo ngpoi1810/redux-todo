89bc9c932f57831ca284951425569d8387e79e11
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-08-18 16:54:41 +0700

    commit

---
+++ new-remote-blog/src/pages/blog/blog.service.ts
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const blogApi = createApi({
+  reducerPath: 'blogApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/' }),
+  endpoints: (builder) => ({})
+})
